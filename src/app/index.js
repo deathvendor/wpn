4f74ae3a3d544dd10074c6aa2cbe24cb4483de42
Anton <eisentier.anton@gmail.com>
2020-07-21 17:32:44 +0300

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,5 @@
+let year = prompt ('How old are you?');
+if (year < 18)
+    {alert ('Пошёл нахуй, школьник!'); }
+    else if (year > 18) {alert ('Good for you!'); }
+    else {alert ('Ты наебать меня решил?'); }
